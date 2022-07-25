d1b82239694969301d337e792401a26f85762bba
Tran Hoang Truong Hai <haitht@Saphias-XS-Max.localdomain>
2022-07-25 15:35:53 +0700

    2nd commit

--- vott2yolo.py
+++ vott2yolo.py
@@ -1,6 +1,17 @@
 import csv
-import pandas
+import pandas as pd 
 import os
 
 def load_csv_to_df(file):
-    
+    df = pd.DataFrame()
+    df.read_csv(file)
+    return df
+
+def calculate_rect(x_min, y_min, x_max, y_max):
+    x_center, y_center, width, height = 0.0
+    x_center = (x_min + x_max) / 2 
+    y_center = (y_min + y_max) / 2
+    width = x_max - x_min
+    height = y_max - y_min
+    return (x_center, y_center, width, height)    
+
